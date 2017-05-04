a25af7386bf01c8562962723448e82b027c7cd50
agccie <agossett@cisco.com>
2017-05-04 19:26:11 -0400

    add default instance config files

---
+++ instance/config.py
@@ -0,0 +1,4 @@
+# configuration file used to override 'config.py' in the base directory.
+# values in this directory will not be committed to the repository so should
+# be safe to store sensitive information.
+# see: https://exploreflask.com/configuration.html

3a1d9ec8ad27700cb1fcb424e7a026546acac040
Hrishikesh <hrdeshpa@cisco.com>
2018-10-17 17:30:50 -0400

    updated cisco ui to latest version and added more ui features

--- UIAssets/src/app/fabrics/fabrics.component.ts
+++ UIAssets/src/app/fabrics/fabrics.component.ts
@@ -25,7 +25,12 @@ export class FabricsComponent implements OnInit {
     this.sorts = { name:"fabric", dir:'asc'} ;
     this.rows = [{fabric:'Fabric1' , status:'Stopped', ips:'2300', macs:'2000', 
                  events:[{time:new Date() , status:'Initializing', description:'Connecting to APIC'},{time:new Date(), status:'Restarting' , description:'User triggered restart'}]}]
-    this.tabs = [{name:'Fabrics',path:'fabric-overview'},{name:'Endpoints',path:'endpoints'},{name:'Latest Events',path:'latest-events'},{name:'Moves',path:'moves'},{name:'Off-subnet Endpoints',path:'offsubnet-endpoints'},{name:'Stale Endpoints', path:'stale-endpoints'}]
+    this.tabs = [{name:'Fabrics',path:'fabric-overview'},
+    {name:'Endpoints',path:'endpoints'},
+    {name:'Latest Events',path:'latest-events'},
+    {name:'Moves',path:'moves'},
+    {name:'Off-subnet Endpoints',path:'offsubnet-endpoints'},
+    {name:'Stale Endpoints', path:'stale-endpoints'}] ;
     this.latestEvents = [{time:new Date() , fabric:'Fabric1', type:'IP', address:'172.168.0.1',vrfbd:'uni/tn-2/ctx-213'},
                          {time:new Date() , fabric:'Fabric1', type:'MAC', address:'0C:09:A4:FF',vrfbd:'uni/tn-2/ctx-213'}] ;
     this.endpointMoves = [{time:new Date() , fabric:'Fabric1', type:'IP', address:'172.168.0.1',vrfbd:'uni/tn-2/ctx-214'},

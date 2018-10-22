e70702962ea8b022a3471931bbd45a55e5136d7f
Andy Gossett <agossett@cisco.com>
2018-10-21 22:40:07 -0400

    add support for clear ip and mac endpoint

--- Service/app/models/aci/worker.py
+++ Service/app/models/aci/worker.py
@@ -5,8 +5,15 @@
     @author agossett@cisco.com
 """
 
-from .. utils import get_app, get_db, setup_logger
-import logging, sys, traceback
+from .. utils import get_app
+from .. utils import get_db
+from .. utils import setup_logger
+from . utils import clear_endpoint
+
+import logging
+import re
+import sys
+import traceback
 
 # module level logging
 logger = logging.getLogger(__name__)
@@ -25,54 +32,6 @@ def db_is_alive():
     logger.error("failed to connect to database")
     return False
 
-def start_monitor(fabric):
-    """ start fabric monitor 
-        TODO.... This is specific to your app..
-    """
-    return False
-
-def stop_monitor(fabric):
-    from .fabrics import get_fabric_processes
-    from .utils import terminate_pid
-    processes = get_fabric_processes() 
-    logger.debug("fabric proceses: %s", processes)
-    pids = processes.get(fabric, [])
-    for p in pids: terminate_pid(p)
-    return True
-
-def start_all_monitor(conditional): 
-    # start all fabric monitors
-    if conditional:
-        app = get_app()
-        if not app.config.get("AUTO_START_MONITOR", False):
-            logger.debug("skipping start all as AUTO_START_MONITOR is disabled")
-            return
-    return execute_all_monitor("start")
-
-def stop_all_monitor(conditional): 
-    # stop all fabric monitors
-    if conditional:
-        app = get_app()
-        if not app.config.get("AUTO_START_MONITOR", False):
-            logger.debug("skipping stop all as AUTO_START_MONITOR is disabled")
-            return
-    return execute_all_monitor("stop")
- 
-def execute_all_monitor(action):
-    # start or stop all monitors via provided via rest interface
-    # this function returns once start/stop is triggered in background
-    from .fabric import (Fabric, rest_fabric_action)
-
-    if action != "start": action = "stop"
-    reason = "%s all fabrics" % action
-    all_fabrics = Fabric.read(_filters={})
-    if "objects" in all_fabrics and len(all_fabrics["objects"])>0:
-        for f in all_fabrics["objects"]:
-            logger.debug("starting fabric %s", f["fabric"])
-            if not rest_fabric_action(f["fabric"], action, reason):
-                logger.warn("failed to %s fabric: %s", action, f)
-    return True
-
 def get_args():
     """ get arguments for worker """
     import argparse
@@ -87,18 +46,18 @@ def get_args():
         )
     parser.add_argument("--check_db", action="store_true", dest="check_db",
         help="validate successful database connection")
-    parser.add_argument("--start", dest="start", type=str, 
-        help="start fabric monitor for specified fabric name")
-    parser.add_argument("--stop", dest="stop", type=str,
-        help="stop fabric monitor for specified fabric name")
-    parser.add_argument("--all_stop", action="store_true", dest="all_stop",
-        help="stop all fabric monitors")
-    parser.add_argument("--all_start", action="store_true", dest="all_start", 
-        help="start all fabric monitors")
-    parser.add_argument("--all_conditional", action="store_true",
-        dest="all_conditional", help=allConditionalHelp)
     parser.add_argument("--stdout", dest="stdout", action="store_true",
         help="send logger output to stdout")
+    subparsers = parser.add_subparsers(title="worker options", dest="worker_op")
+    parser1 = subparsers.add_parser("clear", help="clear endpoint in fabric")
+    parser1.add_argument("--fabric", required=True, dest="fabric", help="fabric name")
+    parser1.add_argument("--pod", required=True, type=int, dest="pod", help="pod id")
+    parser1.add_argument("--node", required=True, type=int, dest="node", help="node id")
+    parser1.add_argument("--addr", required=True, dest="addr", help="mac, ipv4, or ipv6 address")
+    parser1.add_argument("--addr_type", dest="addr_type", help="address type (ip or mac)",
+            choices=["mac","ip"])
+    parser1.add_argument("--vnid", required=True, type=int, dest="vnid", help="vrf/bd vnid")
+    parser1.add_argument("--vrf_name", dest="vrf_name", help="vrf name", default="")
     args = parser.parse_args()
     return args
 
@@ -112,10 +71,8 @@ if __name__ == "__main__":
     stdout = args.stdout
     fname="worker.log"
     debug_modules = [
-        "app.models.aci.nodes",
-        "app.models.aci.subscription_ctrl", 
         "app.models.aci.utils",
-        "app.models.aci.fabrics",
+        #"app.models.aci.tools.connection",
     ]
     setup_logger(logger,fname=fname,stdout=stdout,quiet=True,thread=True)
     for l in debug_modules:
@@ -125,24 +82,13 @@ if __name__ == "__main__":
     if args.check_db: 
         logger.debug("worker request: check_db")
         method = db_is_alive
-    elif args.all_start:
-        logger.debug("start all fabric monitors")
-        method = start_all_monitor
-        method_args = [args.all_conditional]
-    elif args.all_stop:
-        logger.debug("stop all fabric monitors")
-        method = stop_all_monitor
-        method_args = [args.all_conditional]
-    elif args.start is not None:
-        logger.debug("start monitor request for fabric: %s" % args.start)
-        method = start_monitor
-        method_args = [args.start]
-    elif args.stop is not None:
-        logger.debug("stop monitor request for fabric: %s" % args.stop)
-        method = stop_monitor
-        method_args = [args.stop]
+    elif args.worker_op == "clear":
+        logger.debug("worker request: clear endpoint")
+        method = clear_endpoint
+        method_args = [args.fabric, args.pod, args.node, args.vnid, args.addr, 
+                args.addr_type, args.vrf_name]
     else:
-        logger.warn("no action provided.  use -h for help")
+        logger.warn("no action provided. use -h for help")
         sys.exit(1)
 
     # execute method with required arguments and exit with appropriate exit code
